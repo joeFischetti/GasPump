17c40b71ee49029787c903ef672feadfdc08586a
Joe Fischetti <jkfischetti@gmail.com>
2019-09-27 12:31:30 -0400

    Updated code with licensing feature... Licenses applied per year

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 # compile script to save a few keystrokes to compile at the command line
 
-javac -cp lib/jssc.jar src/*.java -d bin
+javac -cp lib/jssc.jar:lib/commons-net-3.6.jar src/*.java -d bin
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 #  script for running the app as root required for the serial port on my system
 
-sudo java -cp lib/sqlite-jdbc-3.8.7.jar:lib/jssc.jar:bin GasPump
+sudo java -cp lib/sqlite-jdbc-3.8.7.jar:lib/jssc.jar:lib/commons-net-3.6.jar:bin GasPump
